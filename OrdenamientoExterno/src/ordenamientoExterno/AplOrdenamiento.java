15d52534c593a420acdafde21b31faffcc7f6fc4
Unknown <emanuel.l4168@gmail.com>
2018-12-08 07:22:02 -0700

    Clase Rutinasa creada
    Se creó la clase Rutinas, con métodos estáticos auxiliares

--- OrdenamientoExterno/src/ordenamientoExterno/AplOrdenamiento.java
+++ OrdenamientoExterno/src/ordenamientoExterno/AplOrdenamiento.java
@@ -2,4 +2,12 @@ package ordenamientoExterno;
 
 public class AplOrdenamiento {
 
+	
+	private AplOrdenamiento() {
+		
+	}
+	
+	public static void main(String [] a) {
+		new AplOrdenamiento();
+	}
 }
